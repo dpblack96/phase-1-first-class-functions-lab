c9bd2fd8aca12c2f84ece667eb37f359b02554fb
Dominic Black <dpblack96@gmail.com>
2022-04-05 15:35:48 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers) {
+    let removedDrivers = []
+    removedDrivers.push(drivers[0])
+    removedDrivers.push(drivers[1])
+    console.log(removedDrivers)
+    return removedDrivers
+};
+
+const returnLastTwoDrivers = function(drivers) {
+    let removedDrivers = []
+    removedDrivers.push(drivers[2])
+    removedDrivers.push(drivers[3])
+    console.log(removedDrivers)
+    return removedDrivers
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+
+const createFareMultiplier = function(multiplier) {
+    return function (fare) {
+        return multiplier * fare
+    }
+}
+
+const fareDoubler = function(fare) {
+    return  createFareMultiplier(2)(fare)
+}
+
+const fareTripler = function(fare) {
+    return  createFareMultiplier(3)(fare)
+}
+
+const selectDifferentDrivers = function (drivers, funct) {
+    return funct(drivers)
+}
+    
